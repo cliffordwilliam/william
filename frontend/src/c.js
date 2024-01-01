d0ea9dcdf91d51a846b521f4c93422157c2e7bb3
Clifford William <ccliffordwilliam@gmail.com>
2024-01-01 19:54:46 +0800

    fix(typo base): extra /

--- frontend/src/c.js
+++ frontend/src/c.js
@@ -2,6 +2,6 @@ export default class c {
   static baseUrl = "http://localhost:3000";
   // static baseFrontendUrl = "http://localhost:5173";
   static baseFrontendUrl =
-    "https://william-2jqoog1ew-cliffordwilliams-projects.vercel.app/";
+    "https://william-2jqoog1ew-cliffordwilliams-projects.vercel.app";
   static baseSpotifyUrl = `https://accounts.spotify.com/authorize?client_id=c427c4dced8b46f092bea016eb4ab5a0&response_type=code&redirect_uri=${c.baseFrontendUrl}/spotify&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`;
 }
