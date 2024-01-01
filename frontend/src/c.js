037d731ff684ad6caa50058873172cc68bdddb19
Clifford William <ccliffordwilliam@gmail.com>
2024-01-01 19:17:20 +0800

    fix(spotify const): base url

--- frontend/src/c.js
+++ frontend/src/c.js
@@ -1,5 +1,4 @@
 export default class c {
   static baseUrl = "http://localhost:3000";
-  static baseSpotifyUrl =
-    `https://accounts.spotify.com/authorize?client_id=c427c4dced8b46f092bea016eb4ab5a0&response_type=code&redirect_uri=${baseUrl}/spotify&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`;
+  static baseSpotifyUrl = `https://accounts.spotify.com/authorize?client_id=c427c4dced8b46f092bea016eb4ab5a0&response_type=code&redirect_uri=${c.baseUrl}/spotify&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`;
 }
